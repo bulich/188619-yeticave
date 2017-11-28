69c86e267eafc5dcb6c27b3af4681cb830cba28b
bulich <garifb@gmail.com>
2017-11-28 20:35:16 +0300

    Исправил ошибки

--- functions.php
+++ functions.php
@@ -30,4 +30,15 @@
       }
   }
 
+  function searchUserByEmail($email, $users) {
+	$result = null;
+	foreach ($users as $user) {
+		if ($user['email'] == $email) {
+			$result = $user;
+			break;
+		}
+	}
+
+	return $result;
+}
 ?>
