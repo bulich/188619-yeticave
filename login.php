6b1d1caeb7787212a0751b8cdf787589f613a5df
bulich <garifb@gmail.com>
2017-11-28 20:35:45 +0300

    Исправил ошибки

---
+++ login.php
@@ -0,0 +1,52 @@
+<?php
+require_once('functions.php');
+require_once('userdata.php');
+require_once('userdata.php');
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$form = $_POST;
+
+	$required = ['email', 'password'];
+	$dict = ['email' => 'Email', 'password' => 'Пароль'];
+	$errors = [];
+	foreach ($_POST as $key => $value) {
+		if (in_array($key, $required)) {
+			if (!$value) {
+				$errors[$dict[$key]] = 'Это поле надо заполнить';
+			}
+		}
+	}
+
+	if ($user = searchUserByEmail($form['email'], $users)) {
+		if (password_verify($form['password'], $user['password'])) {
+			$_SESSION['user'] = $user;
+		}
+		else {
+			$errors[$dict['password']] = 'Неверный пароль';
+		}
+	}
+	else {
+		$errors[$dict['email']] = 'Такой пользователь не найден';
+	}
+
+	if (count($errors)) {
+		$page_content = renderTemplate('login', ['form' => $form, 'errors' => $errors]);
+	}
+	else {
+		header("Location: /index.php");
+		exit();
+	}
+}
+else {
+	$page_content = renderTemplate('login', []);
+}
+
+$layout_content = renderTemplate('layout', [
+	'content'    => $page_content,
+	'categories' => [],
+	'title'      => 'GifTube - Вход на сайт'
+]);
+
+print($layout_content);
