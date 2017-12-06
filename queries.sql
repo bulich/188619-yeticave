9e050a83ab49e212856172cc626d7fcdf9867d7d
bulich <garifb@gmail.com>
2017-12-06 21:58:58 +0300

    Исправил ошибки

--- queries.sql
+++ queries.sql
@@ -1,5 +1,5 @@
 /* Категории */
-INSERT INTO categories (name)
+INSERT INTO categories (category_title)
   VALUES ('Доски и лыжи'),
   VALUES ('Крепления'),
   VALUES ('Ботинки'),
@@ -8,58 +8,58 @@ INSERT INTO categories (name)
   VALUES ('Инструменты');
 
 /* Пользователи*/
-INSERT INTO users (reg_date, email, username, pass, avatar, contacts, lots_id, bets_id)
-  VALUES ('2017-08-08', 'ignat.v@gmail.com', 'Игнат', '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka', 'img/user.jpg', 'ignat.v@gmail.com', 0, 2),
-  VALUES ('2017-10-02', 'kitty_93@li.ru', 'Леночка', '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa', 'img/user.jpg', 'kitty_93@li.ru', 1, 1),
-  VALUES ('2017-05-15', 'warrior07@mail.ru', 'Руслан', '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW', 'img/user.jpg', 'kitty_93@li.ru', 2, 0);
+INSERT INTO users (reg_date, email, username, pass, avatar, contacts)
+  VALUES ('2017-08-08 00:00:00', 'ignat.v@gmail.com', 'Игнат', '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka', 'img/user.jpg', 'ignat.v@gmail.com'),
+  VALUES ('2017-10-02 00:00:00', 'kitty_93@li.ru', 'Леночка', '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa', 'img/user.jpg', 'kitty_93@li.ru'),
+  VALUES ('2017-05-15 00:00:00', 'warrior07@mail.ru', 'Руслан', '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW', 'img/user.jpg', 'kitty_93@li.ru');
 
 /* Лоты*/
-INSERT INTO lots (create_date, name, message, img, rate, end_date, step, author_id, winner_id, category_id, is_open)
-  VALUES ('2017-12-01', '2014 Rossignol District Snowboard', '2014 Rossignol District Snowboard', 'lot-1.jpg', 10000, '2017-12-21', 1000, 1, 2, 0, 1),
-  VALUES ('2017-12-01', 'DC Ply Mens 2016/2017 Snowboard', 'DC Ply Mens 2016/2017 Snowboard', 'lot-2.jpg', 5000, '2017-12-30', 500, 2, 0, 0, 1),
-  VALUES ('2017-12-01', 'Крепления Union Contact Pro 2015 года размер L/XL', 'Крепления Union Contact Pro 2015 года размер L/XL', 'lot-3.jpg', 6000, '2017-12-19', 250, 0, 1, 1, 1),
-  VALUES ('2017-12-01', 'Ботинки для сноуборда DC Mutiny Charocal', 'Ботинки для сноуборда DC Mutiny Charocal', 'lot-4.jpg', 7999, '2017-12-12', 500, 2, 1, 2, 1),
-  VALUES ('2017-12-01', 'Куртка для сноуборда DC Mutiny Charocal', 'Куртка для сноуборда DC Mutiny Charocal', 'lot-5.jpg', 8600, '2017-12-15', 250, 0, 2, 3, 1),
-  VALUES ('2017-12-01', 'Маска Oakley Canopy', 'Маска Oakley Canopy', 'lot-6.jpg', 3200, '2017-12-16', 320, 1, 0, 4, 1),
+INSERT INTO lots (create_date, name, description, image_path, rate, end_date, step, author_id, winner_id, category_id)
+  VALUES ('2017-12-01 00:00:00', '2014 Rossignol District Snowboard', '2014 Rossignol District Snowboard', 'lot-1.jpg', 10000, '2017-12-21 00:00:00', 1000, 1, 2, 0),
+  VALUES ('2017-12-01 00:00:00', 'DC Ply Mens 2016/2017 Snowboard', 'DC Ply Mens 2016/2017 Snowboard', 'lot-2.jpg', 5000, '2017-12-30 00:00:00', 500, 2, 0, 0),
+  VALUES ('2017-12-01 00:00:00', 'Крепления Union Contact Pro 2015 года размер L/XL', 'Крепления Union Contact Pro 2015 года размер L/XL', 'lot-3.jpg', 6000, '2017-12-19 00:00:00', 250, 0, 1, 1),
+  VALUES ('2017-12-01 00:00:00', 'Ботинки для сноуборда DC Mutiny Charocal', 'Ботинки для сноуборда DC Mutiny Charocal', 'lot-4.jpg', 7999, '2017-12-12 00:00:00', 500, 2, 1, 2),
+  VALUES ('2017-12-01 00:00:00', 'Куртка для сноуборда DC Mutiny Charocal', 'Куртка для сноуборда DC Mutiny Charocal', 'lot-5.jpg', 8600, '2017-12-15 00:00:00', 250, 0, 2, 3),
+  VALUES ('2017-12-01 00:00:00', 'Маска Oakley Canopy', 'Маска Oakley Canopy', 'lot-6.jpg', 3200, '2017-12-16 00:00:00', 320, 1, 0, 4),
 
 /*Ставки*/
 INSERT INTO bets (create_date, price, author_id, lot_id)
-  VALUES ('2017-12-02', 10000, 1, 2)
-  VALUES ('2017-12-02', 12000, 0, 5)
+  VALUES ('2017-12-02 00:00:00', 10000, 1, 2),
+  VALUES ('2017-12-02 00:00:00', 12000, 0, 5);
 
 
 /*Объединения*/
-SELECT lots.id, lots.author_id FROM lots
+SELECT lot_id, create_date, name, image_path end_date, author_id, winner_id FROM lots
 JOIN users
 ON lots.author_id = users.id;
 
-SELECT lots.id, lots.winner_id FROM lots
+SELECT lot_id, create_date, name, image_path end_date, author_id, winner_id FROM lots
 JOIN users
 ON lots.winner_id = users.id;
 
-SELECT lots.id, lots.category_name FROM lots
+SELECT lot_id, create_date, name, image_path end_date, author_id, winner_id FROM lots
 JOIN categories
-ON lots.category_id = categories.id;
+ON lots.category_id = categories.category_id;
 
-SELECT bets.id, bets.author_id FROM bets
+SELECT create_date, price, author_id, lot_id FROM bets
 JOIN users
 ON bets.author_id = users.id;
 
-SELECT bets.id, bets.lot_id FROM bets
+SELECT create_date, price, author_id, lot_id FROM bets
 JOIN lots
-ON bets.lot_id = lots.id;
+ON bets.lot_id = lots.lot_id;
 
 /*получить список из всех категорий*/
 
-SELECT * FROM categories;
+SELECT category_title FROM categories;
 
 /*получить самые новые, открытые лоты*/
 
-SELECT * FROM lots WHERE is_open = 1;
+SELECT lot_id, create_date, name, image_path end_date, author_id, winner_id FROM lots WHERE DATE(create_date) BETWEEN '2017-12-06 00:00:00' AND NOW();;
 
 /*найти лот по его названию или описанию*/
 
-SELECT * FROM lots WHERE name = 'Куртка для сноуборда DC Mutiny Charocal';
+SELECT lot_id, create_date, name, image_path end_date, author_id, winner_id FROM lots WHERE name = 'Куртка для сноуборда DC Mutiny Charocal';
 
 /*обновить название лота по его идентификатору*/
 
--- schema.sql
+++ schema.sql
@@ -3,28 +3,27 @@ CREATE DATABASE yeticave COLLATE utf8_general_ci;
 USE yeticave;
 
 CREATE TABLE IF NOT EXISTS categories (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name CHAR(128)
+  category_id INT AUTO_INCREMENT PRIMARY KEY,
+  category_title VARCHAR(128)
 ) ENGINE=INNODB CHARACTER SET=utf8;
 
 CREATE TABLE IF NOT EXISTS lots (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  create_date DATE,
-  name CHAR(128),
-  message TEXT(500),
-  img CHAR(128),
+  lot_id INT AUTO_INCREMENT PRIMARY KEY,
+  create_date DATETIME,
+  name VARCHAR(128),
+  description TEXT(500),
+  image_path VARCHAR(128),
   rate INT,
-  end_date DATE,
+  end_date DATETIME,
   step INT,
   author_id INT,
   winner_id INT,
-  category_id INT,
-  is_open INT
+  category_id INT
 ) ENGINE=INNODB CHARACTER SET=utf8;
 
 CREATE TABLE IF NOT EXISTS bets (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  create_date DATE,
+  bet_id INT AUTO_INCREMENT PRIMARY KEY,
+  create_date DATETIME,
   price DECIMAL,
   author_id INT,
   lot_id INT
@@ -32,30 +31,26 @@ CREATE TABLE IF NOT EXISTS bets (
 
 CREATE TABLE IF NOT EXISTS users (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  reg_date DATE,
-  email CHAR(128),
+  reg_date DATETIME,
+  email VARCHAR(128),
   username CHAR(128),
   pass CHAR(64),
-  avatar CHAR(255),
-  contacts TEXT(300),
-  lots_id INT,
-  bets_id INT,
-  FOREIGN KEY (lots_id) REFERENCES lots(id),
-  FOREIGN KEY (bets_id) REFERENCES bets(id)
+  avatar VARCHAR(255),
+  contacts TEXT(300)
 ) ENGINE=INNODB CHARACTER SET=utf8;
 
 ALTER TABLE lots
   ADD FOREIGN KEY (author_id) REFERENCES users(id),
   ADD FOREIGN KEY (winner_id) REFERENCES users(id),
-  ADD FOREIGN KEY (category_id) REFERENCES categories(id);
+  ADD FOREIGN KEY (category_id) REFERENCES categories(category_id);
 
 ALTER TABLE bets
  ADD FOREIGN KEY (author_id) REFERENCES users(id),
- ADD FOREIGN KEY (lot_id) REFERENCES lots(id);
+ ADD FOREIGN KEY (lot_id) REFERENCES lots(lot_id);
 
 
-CREATE INDEX lot_id ON lots(id);
+CREATE INDEX lot_id ON lots(lot_id);
 CREATE INDEX lot_end ON lots(end_date);
-CREATE INDEX bet_id ON bets(id);
+CREATE INDEX bet_id ON bets(bet_id);
 CREATE INDEX user_id ON users(id);
 CREATE INDEX user_email ON users(email);
